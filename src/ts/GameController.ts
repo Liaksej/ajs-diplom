12803cdec7e87da9e0a5c9af19599798abb9696c
Liaksej <tarusov.liaksej@gmail.com>
2023-08-27 17:13:01 +0200

    Add team creation functionality to GameController
    A new method to generate teams (gamerTeam, enemyTeam) was added to GameController.ts. We've included a helper function called getTeamCells that calculates the cells for each team based on the role. This addition enables our GameController to handle the creation and positioning of teams within our game, enhancing gameplay logic and efficiency.

--- src/ts/GameController.ts
+++ src/ts/GameController.ts
@@ -1,9 +1,11 @@
 import GamePlay from "./GamePlay";
 import themes from "./themes";
+import PositionedCharacter from "./PositionedCharacter";
 
 export default class GameController {
   gamePlay: GamePlay;
   stateService: any;
+
   constructor(gamePlay: GamePlay, stateService: any) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
@@ -25,5 +27,27 @@ export default class GameController {
 
   onCellLeave(index: number) {
     // TODO: react to mouse leave
+  }
+
+  creatTeams() {
+    const gamerTeam;
+    const enemyTeam;
+    const gamerTeamCells: number[] = getTeamCells(64, 8, "gamer");
+    const enemyTeamCells: number[] = getTeamCells(64, 8, "enemy");
+
+    function getTeamCells(allCells: number, rowCells: number, role: string) {
+      const teamCells: number[] = [];
+      const rows = allCells / rowCells;
+      for (let i = 0; i < rows; i++) {
+        if (role === "gamer") {
+          teamCells.push(...[allCells - rowCells - 2, allCells - rowCells - 2]);
+        }
+        if (role === "enemy") {
+          teamCells.push(...[allCells, allCells - 1]);
+        }
+      }
+      allCells = allCells - rowCells;
+      return teamCells;
+    }
   }
 }
