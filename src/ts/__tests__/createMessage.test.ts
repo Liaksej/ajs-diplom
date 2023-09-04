534408fbadbefdaf9d02f34571d12bfe51069240
Liaksej <tarusov.liaksej@gmail.com>
2023-09-04 12:45:41 +0200

    Add tests for createMessage method in GameController
    Introduced unit tests for the method 'createMessage' in GameController. The tests ensure that a correct message is created for a character at a given position. Also added a test case for a position with no character, ensuring the method properly handles such scenarios.

---
+++ src/ts/__tests__/createMessage.test.ts
@@ -0,0 +1,36 @@
+import GameController from "../GameController";
+import GamePlay from "../GamePlay";
+import GameStateService from "../GameStateService";
+import { Bowman } from "../characters/Bowman";
+
+let gameCtrl: GameController;
+let gamePlay: GamePlay;
+let stateService: GameStateService;
+
+beforeAll(() => {
+  gameCtrl = new GameController(gamePlay, stateService);
+});
+
+test("Method createMessage should create a correct message for character at given position", () => {
+  gameCtrl["positions"] = [
+    {
+      character: new Bowman(1),
+      position: 5,
+    },
+  ];
+  expect(gameCtrl["createMessage"](5)).toBe(
+    "\u{1F396} 1 \u{2694} 25 \u{1F6E1} 25 \u{2764} 50",
+  );
+});
+
+test("Method createMessage should create a correct message for character at given position", () => {
+  gameCtrl["positions"] = [
+    {
+      character: new Bowman(1),
+      position: 5,
+    },
+  ];
+  expect(gameCtrl["createMessage"](6)).toBe(
+    "\u{1F396} undefined \u{2694} undefined \u{1F6E1} undefined \u{2764} undefined",
+  );
+});
