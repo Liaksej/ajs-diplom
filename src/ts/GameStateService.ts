8f5a3b55665901f35f457850dbbf456c80b24e74
Liaksej <tarusov.liaksej@gmail.com>
2023-09-23 00:07:27 +0200

    Refactor GameStateService for explicit typing and data encoding
    This update refactors the GameStateService class in TypeScript to provide explicit typing for the game state objects and enhance data handling. Instead of storing the state as a simple JSON string, the state is now converted to binary and stored as a base64 string providing a more efficient storage mechanism. The load-method also has been updated to properly decode the stored state from base64 to binary, then to JSON. This update enhances code readability and data storage efficiency.

--- src/ts/GameStateService.ts
+++ src/ts/GameStateService.ts
@@ -1,18 +1,57 @@
+import GameState from "./GameState";
+import { LevelType } from "./Character";
+import PositionedCharacter from "./PositionedCharacter";
+
+export interface ObjectForStore {
+  turn: "gamer" | "computer";
+  level: LevelType;
+  positions: PositionedCharacter[];
+  isGameEnd: boolean;
+}
+
 export default class GameStateService {
-  storage: any;
-  constructor(storage: any) {
+  storage: Storage;
+  constructor(storage: Storage) {
     this.storage = storage;
   }
 
-  save(state: any) {
-    this.storage.setItem("state", JSON.stringify(state));
+  save(state: ObjectForStore) {
+    try {
+      const data = JSON.stringify(state);
+
+      const binaryArray = new Uint8Array(new ArrayBuffer(data.length));
+      for (let i = 0; i < data.length; i++) {
+        binaryArray[i] = data.charCodeAt(i);
+      }
+      const base64Data = btoa(
+        String.fromCharCode.apply(null, Array.from(binaryArray)),
+      );
+      console.log(base64Data);
+      this.storage.setItem("state", base64Data);
+    } catch (e) {
+      throw new Error("Failed to save state: " + e);
+    }
   }
 
   load() {
+    const base64Data = this.storage.getItem("state");
+
+    if (!base64Data) {
+      throw new Error("No state data found in storage");
+    }
+
     try {
-      return JSON.parse(this.storage.getItem("state"));
+      const binaryState = atob(base64Data);
+      const length = binaryState.length;
+      const binaryArray = new Uint8Array(new ArrayBuffer(length));
+      for (let i = 0; i < length; i++) {
+        binaryArray[i] = binaryState.charCodeAt(i);
+      }
+      const data = new TextDecoder().decode(binaryArray);
+      console.log(JSON.parse(data));
+      return JSON.parse(data);
     } catch (e) {
-      throw new Error("Invalid state");
+      throw new Error("Invalid state" + e);
     }
   }
 }
